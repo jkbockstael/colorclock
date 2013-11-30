50989848a69097c1c54082130447005045943a77
Jean-Karim Bockstael <jkb@jkbockstael.be>
2013-11-30 14:27:51 +0100

    Incorporated changes from brnrd/colorclock.
    Incorporated suggested code cleanup from brnrd/colorclock, but sticking
    to Zachas' coding style instead of Crockford's.
    
    Squashed commit of the following:
    
    commit c6be6141f56de932f16bc37ce09c4bc58402ebeb
    Author: Jean-Karim Bockstael <jkb@jkbockstael.be>
    Date:   Sat Nov 30 14:25:03 2013 +0100
    
        This maintainer is more Zachas than Crockford.
    
    commit 703c48a8c420ea5fd14aa1af79e6098e3422a32c
    Author: brnrd <bernard.debecker@gmail.com>
    Date:   Fri Nov 29 23:50:45 2013 +0000
    
        Code cleaning
    
        Code cleaning, a bit grammar nazi style. Removing the anonymous function,
        the semicolons after the functions, single quotes...

--- colorclock.js
+++ colorclock.js
@@ -1,49 +1,59 @@
 // Color Clock JS : JavaScript partial reimplementation of http://thecolourclock.co.uk/ (by Jack Hughes)
 // 2012, Jean-Karim Bockstael <jkb@jkbockstael.be>
-window.cc = (function() {
-	var timer = undefined;
-	var dom = {};
-	var hexaMode = true;
-	var lastTime = "";
-	var _centerDisplay = function() {
-		dom.display.style.fontSize = Math.floor(window.innerHeight / 3.5) + "px";
-		dom.display.style.top = Math.floor((window.innerHeight - dom.display.offsetHeight) / 2) + "px";
-		dom.display.style.width = window.innerWidth;
-	};
-	var _toggleHexa = function() {
-		hexaMode = !hexaMode;
-		clearTimeout(timer);
+window.cc = (function cc() {
+	var _timer = undefined;
+	var _dom = {};
+	var _hexaMode = true;
+	var _lastTime = "";
+
+	function _centerDisplay() {
+		_dom.display.style.fontSize = Math.floor(window.innerHeight / 3.5) + "px";
+		_dom.display.style.top = Math.floor((window.innerHeight - _dom.display.offsetHeight) / 2) + "px";
+		_dom.display.style.width = window.innerWidth;
+	}
+
+	function toggleHexa() {
+		_hexaMode = !_hexaMode;
+		clearTimeout(_timer);
 		_step();
-	};
-	var _step = function() {
+	}
+
+	function _step() {
 		var date = new Date();
-		if (date.toString() !== lastTime) {
-			lastTime = date.toString();
+		if (date.toString() !== _lastTime) {
+			_lastTime = date.toString();
 			var hours = date.getHours();
 			var minutes = date.getMinutes();
 			var seconds = date.getSeconds();
+
 			// Update colors
 			var hoursRatio = (hours/23);
 			var minutesRatio = (minutes/59);
 			var secondsRatio = (seconds/59);
+
 			var bodyRed = Math.floor((hoursRatio - (hoursRatio * 10 / 100)) * 256);
 			var bodyGreen = Math.floor((minutesRatio - (minutesRatio * 10 / 100)) * 256);
 			var bodyBlue = Math.floor((secondsRatio - (secondsRatio * 10 / 100)) * 256);
+
 			var displayRed = Math.floor((hoursRatio - (hoursRatio * 20 / 100)) * 256);
 			var displayGreen = Math.floor((minutesRatio - (minutesRatio * 20 / 100)) * 256);
 			var displayBlue = Math.floor((secondsRatio - (secondsRatio * 20 / 100)) * 256);
+
 			var shadowRed = Math.floor((hoursRatio - (hoursRatio * 30 / 100)) * 256);
 			var shadowGreen = Math.floor((minutesRatio - (minutesRatio * 30 / 100)) * 256);
 			var shadowBlue = Math.floor((secondsRatio - (secondsRatio * 30 / 100)) * 256);
+
 			var highlightRed = Math.floor(hoursRatio * 256);
 			var highlightGreen = Math.floor(minutesRatio * 256);
 			var highlightBlue = Math.floor(secondsRatio * 256);
-			dom.body.style.backgroundColor = "rgb(" + bodyRed + "," + bodyGreen + "," + bodyBlue + ")";
-			dom.display.style.color = "rgb(" + displayRed + "," + displayGreen + "," + displayBlue + ")";
-			dom.display.style.textShadow = "-1px -2px 2px rgb(" + shadowRed + "," + shadowGreen + "," + shadowBlue + ")";
-			dom.display.style.textHighlight = "1px 2px 2px rgb(" + highlightRed + "," + highlightGreen + "," + highlightBlue + ")";
+
+			_dom.body.style.backgroundColor = "rgb(" + bodyRed + "," + bodyGreen + "," + bodyBlue + ")";
+			_dom.display.style.color = "rgb(" + displayRed + "," + displayGreen + "," + displayBlue + ")";
+			_dom.display.style.textShadow = "-1px -2px 2px rgb(" + shadowRed + "," + shadowGreen + "," + shadowBlue + ")";
+			_dom.display.style.textHighlight = "1px 2px 2px rgb(" + highlightRed + "," + highlightGreen + "," + highlightBlue + ")";
+
 			// Display time
-			if (hexaMode === true) {
+			if (_hexaMode === true) {
 				var time = "" + ((hours < 16) ? "0" + hours.toString(16) : hours.toString(16));
 				time += ":" + ((minutes < 16) ? "0" + minutes.toString(16) : minutes.toString(16));
 				time += ":" + ((seconds < 16) ? "0" + seconds.toString(16) : seconds.toString(16));
@@ -53,36 +63,38 @@ window.cc = (function() {
 				time += ":" + ((minutes < 10) ? "0" + minutes : minutes);
 				time += ":" + ((seconds < 10) ? "0" + seconds : seconds);
 			}
-			dom.display.innerHTML = time;
+			_dom.display.innerHTML = time;
 		}
-		timer = setTimeout(_step, 200);
-	};
-	var _start = function() {
-		dom.body = document.getElementsByTagName("body")[0];
-		dom.display = document.createElement("div");
-		dom.display.id = "display";
-		dom.display.style.position = "relative";
-		dom.display.style.fontFamily = "monospace";
-		dom.display.style.textAlign = "center";
-		dom.display.style.width = window.innerWidth;
-		dom.display.style.margin = "0";
-		dom.display.style.marginLeft = "auto";
-		dom.display.style.marginRight = "auto";
-		dom.display.style.color = "transparent";
-		dom.body.style.position = "absolute";
-		dom.body.style.width = "100%";
-		dom.body.style.height = "100%"
-		dom.body.style.margin = "0";
-		dom.display.innerHTML = "00:00:00";
-		dom.display.onclick = _toggleHexa;
-		dom.body.appendChild(dom.display);
+		_timer = setTimeout(_step, 200);
+	}
+
+	function _start() {
+		_dom.body = document.getElementsByTagName("body")[0];
+		_dom.display = document.createElement("div");
+		_dom.display.id = "display";
+		_dom.display.style.position = "relative";
+		_dom.display.style.fontFamily = "monospace";
+		_dom.display.style.textAlign = "center";
+		_dom.display.style.width = window.innerWidth;
+		_dom.display.style.margin = "0";
+		_dom.display.style.marginLeft = "auto";
+		_dom.display.style.marginRight = "auto";
+		_dom.display.style.color = "transparent";
+		_dom.body.style.position = "absolute";
+		_dom.body.style.width = "100%";
+		_dom.body.style.height = "100%"
+		_dom.body.style.margin = "0";
+		_dom.display.innerHTML = "00:00:00";
+		_dom.display.onclick = toggleHexa;
+		_dom.body.appendChild(_dom.display);
 		window.onresize = _centerDisplay;
 		setTimeout(_centerDisplay, 0);
 		setTimeout(_step, 1);
-	};
+	}
+
 	return {
 		start: function() {
 			_start();
 		}
-	};
+	}
 }());
